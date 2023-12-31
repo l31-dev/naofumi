671c5c2fa92629b244a8a92cc0764e19837fb26d
trail-l31 <95828763+trail-l31@users.noreply.github.com>
2023-12-31 05:06:19 +0100

    Fix missing semicolon in getenv function

--- src/utils/environment.ts
+++ src/utils/environment.ts
@@ -1,18 +1,17 @@
-
 /**
  * Retrieves the value of the specified environment variable.
  * Throws an error if the variable is missing or empty.
- * 
+ *
  * @param key - The name of the environment variable.
  * @returns The value of the environment variable.
  * @throws Error if the environment variable is missing or empty.
  */
 export function getenv(key: string): string {
-    const value = process.env[key]
+        const value = process.env[key];
 
-    if (!value || value.length === 0) {
-        throw new Error(`Missing environment variable ${key}`)
-    }
+        if (!value || value.length === 0) {
+                throw new Error(`Missing environment variable ${key}`);
+        }
 
-    return value
+        return value;
 }
