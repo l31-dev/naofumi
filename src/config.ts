7527b81188ec6095c2ecfc645c40d164d34f4223
trail-l31 <95828763+trail-l31@users.noreply.github.com>
2023-12-31 05:59:46 +0100

    refactor(config): update Config class for readability and maintainability
    Refactored the Config class in 'config.ts' to improve code readability and maintainability. Adjusted indentation for better consistency and clarity. Introduced explicit typing and modified the constructor to include the 'guildID' property initialization alongside 'token'. Additionally, added missing newline at the end of the file for consistency.
    
    contact@l31.dev

--- src/config.ts
+++ src/config.ts
@@ -1,24 +1,26 @@
-import { getenv } from "@utils/environment";
+import { getenv } from '@utils/environment';
 
 export class Config {
-    private static instance: Config;
+        private static instance: Config;
 
-    public token: string;
-
-    private constructor() {
-        this.token = getenv('TOKEN');
-    }
-
-    /**
-     * Returns the singleton instance of the Config class.
-     * If the instance does not exist, it creates a new one.
-     * @returns The singleton instance of the Config class.
-     */
-    public static getInstance(): Config {
-        if (!Config.instance) {
-            Config.instance = new Config();
+        public token: string;
+        public guildID: string;
+        
+        private constructor() {
+                this.token = getenv('TOKEN');
+                this.guildID = getenv('GUILD_ID');
         }
 
-        return Config.instance;
-    }
+        /**
+         * Returns the singleton instance of the Config class.
+         * If the instance does not exist, it creates a new one.
+         * @returns The singleton instance of the Config class.
+         */
+        public static getInstance(): Config {
+                if (!Config.instance) {
+                        Config.instance = new Config();
+                }
+
+                return Config.instance;
+        }
 }
