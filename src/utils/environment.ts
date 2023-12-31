48a6ffda96fdf1110c5669d3ef69a8a17bd88df6
trail-l31 <95828763+trail-l31@users.noreply.github.com>
2023-12-31 04:03:15 +0100

    Add getenv function to retrieve environment variables

---
+++ src/utils/environment.ts
@@ -0,0 +1,18 @@
+
+/**
+ * Retrieves the value of the specified environment variable.
+ * Throws an error if the variable is missing or empty.
+ * 
+ * @param key - The name of the environment variable.
+ * @returns The value of the environment variable.
+ * @throws Error if the environment variable is missing or empty.
+ */
+export function getenv(key: string): string {
+    const value = process.env[key]
+
+    if (!value || value.length === 0) {
+        throw new Error(`Missing environment variable ${key}`)
+    }
+
+    return value
+}
