7434e13f5f51f4d4ad939d659165016dd75827ed
trail-l31 <95828763+trail-l31@users.noreply.github.com>
2023-12-31 06:49:57 +0100

    feat: Add PingCommand and handle bot initialization
    Added the PingCommand module to handle ping functionality within the bot. Also, implemented bot initialization by importing necessary modules such as Bot, Config, and Client from Discord.js. Configuration settings are now properly initialized and utilized within the bot's functionality. The bot now listens for the 'ready' event, adds the PingCommand to the specified guild, and starts handling commands accordingly. This commit ensures the bot is fully operational and logged in using the provided token.
    
    contact@l31.dev

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log("Hello via Bun!");
+import { Bot } from './bot';
+import PingCommand from './commands/ping_command';
+import { Config } from './config';
+import { Client } from 'discord.js';
+
+const config: Config = Config.getInstance();
+const bot: Bot = new Bot();
+
+bot.on('ready', () => {
+        bot.addCommand(new PingCommand(), config.guildID);
+        bot.handleCommands();
+})
+
+bot.login(config.token);
