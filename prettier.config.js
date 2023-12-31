7b01af558061398cb2082b69122a1cecc5635151
trail-l31 <95828763+trail-l31@users.noreply.github.com>
2023-12-31 05:05:30 +0100

    Remove ESLint and add Prettier configuration files

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+export default {
+        printWidth: 80,
+        tabWidth: 8,
+        useTabs: false,
+        semi: true,
+        singleQuote: true,
+        arrowParens: 'always',
+        bracketSpacing: true,
+        trailingComma: 'all',
+};
